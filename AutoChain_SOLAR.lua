867e8587b58a52ade865ae827147b9fa9113c51e
PetkaML228 <guestslavik@gmail.com>
2026-05-30 11:16:00 +0300

    Update AutoChain_SOLAR.lua

--- AutoChain_SOLAR.lua
+++ AutoChain_SOLAR.lua
@@ -75,7 +75,7 @@ warn("[AutoChain] Инициализация: OK")
 local ABILITY_NAME     = "Call Of Arms"
 local COMMANDER_TYPE   = "Commander"
 local BUFF_DURATION    = 10.0
-local ABILITY_COOLDOWN = 50.0
+local ABILITY_COOLDOWN = 30.0
 local ACTIVATION_LEAD  = 0.1
 
 -- ═══════════════════════════════════════════════════════════════
